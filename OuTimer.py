8018f9b9f32cb66250b92dbc3f884c01002ca8ec
oShine <oyjqdlp@126.com>
2016-11-15 18:00:45 +0800

    v1.0.0.1

---
+++ OuTimer.py
@@ -0,0 +1,29 @@
+import threading
+
+
+class Timer(threading.Thread):
+    def __init__(self, interval, function, args=[], kwargs={}):
+        threading.Thread.__init__(self)
+
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.finished = threading.Event()
+        self.daemon = True
+
+    def run(self):
+        while not self.finished.is_set():
+            try:
+                self.function(*self.args, **self.kwargs)
+            except Exception, ex:
+                self._note("Function:(%s),exception:(%r)", self.function, ex)
+                pass
+            self.finished.wait(self.interval)
+
+    def close(self):
+        self.finished.set()
+
+    def terminate(self):
+        self.close()
+        self.join(self.interval*2)
